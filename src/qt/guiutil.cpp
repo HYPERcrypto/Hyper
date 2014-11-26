4434211ea0f988d5931cb941b85bbaafdce2849d
Palm Detroit <palmdetroit@gmail.com>
2014-11-25 21:18:19 -0500

    URI

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -81,7 +81,7 @@ void setupAmountWidget(QLineEdit *widget, QWidget *parent)
 
 bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 {
-    if(uri.scheme() != QString("bitcoin"))
+    if(uri.scheme() != QString("hyper"))
         return false;
 
     SendCoinsRecipient rv;
@@ -131,13 +131,13 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 
 bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 {
-    // Convert bitcoin:// to bitcoin:
+    // Convert hyper:// to hyper:
     //
-    //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
+    //    Cannot handle this later, because hyper:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith("bitcoin://"))
+    if(uri.startsWith("hyper:// "))
     {
-        uri.replace(0, 10, "bitcoin:");
+        uri.replace(0, 11, "hyper:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
