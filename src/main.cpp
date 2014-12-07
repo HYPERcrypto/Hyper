10ed8f2214beb53def6da8bd45d8797fa2ea4142
Palm Detroit <palmdetroit@gmail.com>
2014-12-07 01:29:55 -0500

    Add -mininput=value option

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@ int64 nHPSTimerStart;
 
 // Settings
 int64 nTransactionFee = MIN_TX_FEE;
-
+int64 nMinimumInputValue = MIN_TX_FEE;
 
 //////////////////////////////////////////////////////////////////////////////
 //
