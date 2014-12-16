439fca3a5e514a5d1fffcf491475afdb6d9d3cf9
Palm Detroit <palmdetroit@gmail.com>
2014-12-16 14:05:13 -0500

    Bump to 1.5.3 Pre Release Beta

--- src/clientversion.h
+++ src/clientversion.h
@@ -8,14 +8,14 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       5
-#define CLIENT_VERSION_REVISION    2
+#define CLIENT_VERSION_REVISION    3
 #define CLIENT_VERSION_BUILD       0
 
 // Updating form 1.5.0 to newer versions will require a new full sync or bootstrap. Please see forum or contact developers.
 // 1.5.1+ will not work with old db files!
 
 // Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE  true
+#define CLIENT_VERSION_IS_RELEASE  false
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
